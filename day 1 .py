034872bd8b8d498199200fece11b3e1497b71a3a
Splashc <plopkoek250@gmail.com>
2022-08-08 23:08:16 +0100

    Initial commit

---
+++ day 1 .py
@@ -0,0 +1,12 @@
+print("Hello world")
+Naam = input("What is your name?\n")
+Lengte = len(Naam)
+print(f'Your Name is {Naam} and is {Lengte} letters long\n')
+print("Welcome to Band name Generator")
+City = input("Where do you live\n")
+petName = input("What is the name of your Pet\n")
+print(f"Your generated band name is {City} {petName}")
+
+
+
+
