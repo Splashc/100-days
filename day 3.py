91b14a29b631aaeb51d7279060896e567477ea6e
Splashc <plopkoek250@gmail.com>
2022-08-10 02:51:55 +0100

    Update day 3.py

---
+++ day 3.py
@@ -0,0 +1,6 @@
+L_bozo = input("JIj bent l bozo want jij kijkt naar coden wie is L bozo? ")
+Noob = input(f"Lol {L_bozo} JIj bent echt noob maar ga zelf je coden maken:")
+
+
+print(f"{L_bozo} JIj hebt gekozen om {Noob} te doen ga dat dan doen godverdommen en ga niet bij anderen kijken")
+
